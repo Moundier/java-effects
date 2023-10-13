8bc6e94c00374c4a6c972b0d8285ccc4e2a803a4
Moundier <garamoundier.developer@protonmail.com>
2023-10-12 22:03:30 -0300

    feat: add user class!

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.net.InetAddress;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @EqualsAndHashCode.Exclude
+    private String username;
+
+    @EqualsAndHashCode.Exclude
+    private Long timestamp;
+
+    @EqualsAndHashCode.Exclude
+    private Status status;
+
+    @EqualsAndHashCode.Include
+    private String inetAddress; // InetAddress 
+
+    public enum Status { IDLE, AVOID, ONLINE }
+}
