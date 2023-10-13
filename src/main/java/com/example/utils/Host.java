5c5abf6a59e3a9158a5a0142a698587bf952c9f8
Moundier <garamoundier.developer@protonmail.com>
2023-10-13 18:46:21 -0300

    chore: host utils!

---
+++ src/main/java/com/example/utils/Host.java
@@ -0,0 +1,17 @@
+package com.example.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Host {
+    
+    public static String getIpAddress() {
+        try {
+            // Get the local host's address and return it as a string
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return "192.168.0.6"; // Default or fallback value
+        }
+    }
+}
