6d17093754ae4f30d0ff5834231f1f6054353da4
Moundier <garamoundier.developer@protonmail.com>
2023-10-13 18:46:44 -0300

    chore: json-user utils!

---
+++ src/main/java/com/example/utils/JsonUser.java
@@ -0,0 +1,29 @@
+package com.example.utils;
+
+import com.example.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUser {
+    
+    public static String serializeUser(User user) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(user);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static User deserializeUser(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(json, User.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
