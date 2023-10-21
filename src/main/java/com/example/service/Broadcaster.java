605b018e074ad5f99eca310df2a68f480f9b8de5
Moundier <garamoundier.developer@protonmail.com>
2023-10-20 22:40:44 -0300

    chore: replace souts by logs!

--- src/main/java/com/example/service/Broadcaster.java
+++ src/main/java/com/example/service/Broadcaster.java
@@ -5,8 +5,12 @@ import java.util.*;
 
 import com.example.model.User;
 import com.example.model.User.Status;
+
 import com.example.utils.JsonUser;
 import com.example.utils.Host;
+import com.example.utils.Console.DONE;
+import com.example.utils.Console.INFO;
+import com.example.utils.Console.LINE;
 
 public class Broadcaster {
 
@@ -30,8 +34,6 @@ public class Broadcaster {
     static Runnable sendRadarMessage = () -> {
         try (DatagramSocket socket = new DatagramSocket()) {
             while (true) {
-                System.out.println("--------------------------------------------------------------");
-                System.out.println("[Socket_START]: Sending to Socket");
                 String radarMessage = JsonUser.serializeUser(user);
                 byte[] sendData = radarMessage.getBytes();
                 InetAddress broadcastAddress = InetAddress.getByName(BROADCAST_IP); // We send in BROAD_CAST
@@ -44,59 +46,60 @@ public class Broadcaster {
         }
     };
 
-    private static int counter = 0; 
+    private static int MONITOR_CYCLE = 0;
+
+    public static int updateCycle () {
+        return MONITOR_CYCLE = MONITOR_CYCLE + 1;
+    }
 
     static Runnable receiveRadarMessages = () -> {
         try (DatagramSocket socket = new DatagramSocket(RADAR_PORT)) {
             while (true) {
-                counter = counter + 1;
-                System.out.println("[Socket_READ]: cycle " + counter);
+                DONE.log("cycle " + updateCycle());
                 byte[] receiveData = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
                 socket.receive(packet);
                 String radarMessage = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("[Sent_DATA_PACKET]: " + radarMessage);
-
+                DONE.log("received: " + radarMessage);
                 User user = JsonUser.deserializeUser(radarMessage);
-                processRadarMessage(user); 
+                processRadarMessage(user);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     };
 
-    static Runnable processRadarMessage(User user) {
-        return () -> {
-            String monitor = String.format("[Watch_MONITOR]: Any User? %s", usersOnline);
-            System.out.println(monitor);
-            // Check if the sender is not the current user
-            if (!user.getInetAddress().equals(Host.getIpAddress())) {
-                usersOnline.add(user);
-                lastRadarMessageTime.put(user, System.currentTimeMillis());
-            }
-        };
+    static void processRadarMessage(User user) {
+
+        INFO.log(String.format("monitor: Found %s", usersOnline));
+        // Check if the sender is not the current user
+        if (!user.getInetAddress().equals(Host.getIpAddress())) {
+            usersOnline.add(user);
+            lastRadarMessageTime.put(user, System.currentTimeMillis());
+        }
     }
 
     static Runnable removeInactiveUsers = () -> {
         try {
             while (true) {
                 long currentTime = System.currentTimeMillis();
-                System.out.println("Current Time in Milliseconds: " + currentTime + "ms");
-    
+                
+                LINE.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                DONE.log("...Epoch Interval(ms): " + currentTime);
                 Set<User> usersToRemove = new HashSet<>();
-    
+
                 for (User user : lastRadarMessageTime.keySet()) {
                     long lastActiveTime = lastRadarMessageTime.get(user);
                     if (currentTime - lastActiveTime > 30000) { // Change to milliseconds
                         usersToRemove.add(user);
                     }
                 }
-    
+
                 for (User user : usersToRemove) {
                     lastRadarMessageTime.remove(user);
                     usersOnline.remove(user);
                 }
-    
+
                 Thread.sleep(5000);
             }
         } catch (InterruptedException e) {
@@ -105,26 +108,27 @@ public class Broadcaster {
     };
 
     public static void initProbe(String username) {
-        
+
+        INFO.log(String.format("building the user %s...", username));
         new Broadcaster(User.builder()
-        .username(username)
-        .inetAddress(Host.getIpAddress())
-        .timestamp(System.currentTimeMillis())
-        .status(Status.ONLINE)
-        .build());
+                .username(username)
+                .inetAddress(Host.getIpAddress())
+                .timestamp(System.currentTimeMillis())
+                .status(Status.ONLINE)
+                .build());
 
+        INFO.log("threading up...");
         List<Thread> thread_list = List.of(
-            new Thread(sendRadarMessage),
-            new Thread(receiveRadarMessages),
-            new Thread(removeInactiveUsers)
-        );
+                new Thread(sendRadarMessage),
+                new Thread(receiveRadarMessages),
+                new Thread(removeInactiveUsers));
 
-        for (Thread thread : thread_list) {
-            thread.start();
-        }
+        INFO.log("threads backgrounding...");
+        for (Thread thread : thread_list) thread.start();
     }
 
     public static void main(String[] args) {
+        // Keep for Debugging
         initProbe("Casanova");
     }
 }
