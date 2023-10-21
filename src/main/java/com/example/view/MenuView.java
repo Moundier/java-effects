e54fa23b74bf8b1f3379c2daeae3d8fb95919ab1
Moundier <garamoundier.developer@protonmail.com>
2023-10-20 22:37:23 -0300

    chore: messaging section!

--- src/main/java/com/example/view/MenuView.java
+++ src/main/java/com/example/view/MenuView.java
@@ -1,114 +1,147 @@
 package com.example.view;
 
-import java.util.List;
-
-import com.example.model.User;
-import com.example.service.Broadcaster;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import com.example.model.User;
+import com.example.service.Broadcaster;
+
+import java.util.List;
+
 public class MenuView extends Application {
 
-	private final Label titleLabel;
-	private static User user;
+    private static User user;
 
-	public MenuView(User user) {
-		MenuView.user = user; 
-		this.titleLabel = new Label("Welcome " + MenuView.user.getUsername());
-	}
+    public MenuView(User user) {
+        MenuView.user = user;
+        Broadcaster.initProbe(user.getUsername()); // Start Broadcast
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-		
-		Broadcaster.initProbe(user.getUsername());
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Tabbed Menu Example");
-		
-		VBox leftMenu = initSideMenu(); // Create a VBox for the left-side menu
-		leftMenu.setPrefWidth(200); // Set the preferred width (e.g., 200 pixels)
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Tabbed Menu Example");
 
-		TabPane tabPane = initTabsMenu(); // Create a TabPane for the top tabbed menu
+        VBox leftMenu = initSideMenu(); // Create a VBox for the left-side menu
+        leftMenu.setPrefWidth(200); // Set the preferred width (e.g., 200 pixels)
 
-		// Create a BorderPane to combine the left menu and tabbed menu
-		BorderPane borderPane = new BorderPane();
-		borderPane.setLeft(leftMenu);
-		borderPane.setCenter(tabPane);
+        TabPane tabPane = initTabsMenu(); // Create a TabPane for the top tabbed menu
 
-		Scene scene = new Scene(borderPane, 800, 600);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        BorderPane borderPane = new BorderPane();
+        borderPane.setLeft(leftMenu);
+        borderPane.setCenter(tabPane);
 
-	private VBox initSideMenu() {
-		// Create VBox
-		VBox leftMenu = new VBox(10);
-		leftMenu.setPadding(new Insets(10));
-	
-		// Testing Styles
-		leftMenu.setStyle(
-			"-fx-background-color: lightgray; " + // Background color
-			"-fx-padding: 10; " +                  // Padding
-			"-fx-border-style: solid inside; " +  // Border style
-			"-fx-border-width: 1; " +             // Border width
-			"-fx-border-insets: 5; " +            // Border insets
-			"-fx-border-radius: 5;"               // Border radius
-		);
-	
-		// Set Style for Label
-		titleLabel.setStyle("-fx-font-weight: bold;"); // Optional: Apply bold font
-	
-		// Add the title label to the VBox
-		leftMenu.getChildren().add(titleLabel);
-	
-		// Create menu items or buttons for the left menu
-		List<Button> btn_list = List.of(
-			new Button("Menu Item 1"),
-			new Button("Menu Item 2"),
-			new Button("Menu Item 3")
-		);
-	
-		// Add Buttons to Menu
-		leftMenu.getChildren().addAll(btn_list);
-	
-		return leftMenu;
-	}	
+        Scene scene = new Scene(borderPane, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-	private TabPane initTabsMenu() {
+    private VBox initSideMenu() {
+        VBox leftMenu = new VBox(10);
+        leftMenu.setPadding(new Insets(10));
 
-		TabPane tabPane = new TabPane();
-		Label label;
-		StackPane pane;
+        leftMenu.setStyle(
+            "-fx-background-color: lightgray; " +
+            "-fx-padding: 10; " +
+            "-fx-border-style: solid inside; " +
+            "-fx-border-width: 1; " +
+            "-fx-border-insets: 5; " +
+            "-fx-border-radius: 5;"
+        );
 
-		List<Tab> tab_list = List.of(
-				new Tab("Tab 1"),
-				new Tab("Tab 2"),
-				new Tab("Tab 3"));
+        Label titleLabel = new Label("Welcome " + MenuView.user.getUsername());
+        titleLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 16;"); // Set font size to 16
 
-		for (Tab tab : tab_list) {
+        leftMenu.getChildren().add(titleLabel);
 
-			int index = (tab_list.indexOf(tab) + 1); // Index of Tab in TabList
+        List<Button> btnList = List.of(
+            new Button("Menu Item 1"),
+            new Button("Menu Item 2"),
+            new Button("Menu Item 3")
+        );
 
-			// Initialize Label and StackPane
-			label = new Label("Content for Tab " + index);
-			pane = new StackPane();
+        for (Button button : btnList) {
+            button.setStyle("-fx-font-size: 16;"); // Set font size to 16 for buttons
+        }
 
-			// Add Pane to Tab
-			pane.setPadding(new Insets(20));
-			pane.getChildren().add(label);
-			tab.setContent(pane);
+        leftMenu.getChildren().addAll(btnList);
 
-			// Add Tab to TabPane
-			tabPane.getTabs().add(tab);
-		}
+        return leftMenu;
+    }
 
-		return tabPane;
-	}
+    private TabPane initTabsMenu() {
+        TabPane tabPane = new TabPane();
+
+        List<Tab> tabList = List.of(
+            new Tab("Tab 1"),
+            new Tab("Tab 2"),
+            new Tab("Tab 3")
+        );
+
+        // Apply font size to tab labels
+        for (Tab tab : tabList) {
+            // tab.setGraphic(new Label(tab.getText()));
+
+            int index = tabList.indexOf(tab) + 1;
+            tab.setContent(createChatTabContent("Tab " + index));
+			tab.setStyle("-fx-font-size: 16;");
+            tabPane.getTabs().add(tab);
+        }
+
+        return tabPane;
+    }
+
+    private BorderPane createChatTabContent(String tabTitle) {
+        BorderPane tabContent = new BorderPane();
+
+        // Create a chat log (TextArea) with scroll bars
+        TextArea chatLog = new TextArea();
+        chatLog.setEditable(false);
+        chatLog.setWrapText(true);
+        chatLog.setStyle("-fx-font-size: 16;"); // Set font size to 16
+
+        ScrollPane scrollPane = new ScrollPane(chatLog);
+        scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+
+        // Create an input field and a send button
+        TextField chatInput = new TextField();
+        chatInput.setPromptText("Type your message...");
+        chatInput.setStyle("-fx-font-size: 16;"); // Set font size to 16
+
+        Button sendButton = new Button("Send");
+        sendButton.setDefaultButton(true);
+        sendButton.setStyle("-fx-font-size: 16;"); // Set font size to 16
+
+        // Create a listener for the send button to add the message to the chat log
+        sendButton.setOnAction(e -> {
+            String message = chatInput.getText();
+            chatLog.appendText("You: " + message + "\n");
+            chatInput.clear();
+        });
+
+        // Create GridPane for Positioning
+        GridPane chatGrid = new GridPane();
+        chatGrid.setHgap(10);
+        chatGrid.setVgap(10);
+        chatGrid.setPadding(new Insets(10));
+
+        chatGrid.add(scrollPane, 0, 0, 1, 1);
+        chatGrid.add(chatInput, 0, 1);
+        chatGrid.add(sendButton, 1, 1);
+
+        // Add the GridPane to the tab content
+        tabContent.setCenter(chatGrid);
+
+        return tabContent;
+    }
 }
