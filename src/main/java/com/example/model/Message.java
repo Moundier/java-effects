e807930250dc20b2e3204d1cba8a6fd7ea241008
Moundier <garamoundier.developer@protonmail.com>
2023-10-22 18:30:52 -0300

    chore: feat Message.java!

---
+++ src/main/java/com/example/model/Message.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    
+    /* Next fix attributes */
+    private String typeof;
+    private String member;
+    private String status;
+}
