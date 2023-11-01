c917861e6cedd6f82358d2eb33cce2a77e3a67b7
Moundier <garamoundier.developer@protonmail.com>
2023-11-01 15:45:00 -0300

    chore: maybe this is disposable!

--- src/main/java/com/example/utils/Console.java
+++ src/main/java/com/example/utils/Console.java
@@ -1,28 +1,57 @@
 package com.example.utils;
 
+
 public class Console {
 
     public static class DONE {
         public static void log(String message) {
-            System.out.println("\u001B[32m [DONE] " + "\u001B[0m" + message ); // Green
+            System.out.println(addGreenColor("[DONE]: ") + message); // Red
         }
     }
 
     public static class INFO {
-        public static void log(String message) {
-            System.out.println("\u001B[33m [INFO] " + "\u001B[0m" + message ); // Yellow
+        public static Void log(String message) {
+            System.out.println(addYellowColor("[INFO]: ") + message); // Red
+            return null;
         }
     }
 
     public static class WARN {
-        public static void log(String message) {
-            System.out.println("\u001B[31m [WARN] " + "\u001B[0m" + message ); // Red
+        public static Void log(String message) {
+            System.out.println(addRedColor("[WARN]: ") + message); // Red
+            return null;
         }
     }
 
     public static class LINE {
-        public static void log(String message) {
-            System.out.println("\u001B[90m [LINE] ["  + message +  "] \u001B[0m"); // Grey
+        public static Void log(String message) {
+            System.out.println(addGreyColor("[LINE]:") + addGreyColor(" [" + message + "]")); // Grey
+            return null;
         }
     }
+
+    public static String addRedColor(String text) {
+        return "\u001B[31m" + text + removeColor(); // ok
+    }
+
+    public static String addGreyColor(String text) {
+        return "\u001B[90m" + text + removeColor(); // ok
+    }
+
+    public static String addGreenColor(String text) {
+        return "\u001B[32m" + text + removeColor(); // ok
+    }
+
+    public static String addYellowColor(String text) {
+        return "\u001B[33m" + text + removeColor();
+    }
+
+    public static String removeColor() {
+        return "\u001B[0m";
+    }
+
+    /* 
+        Todo
+        Iterate through the characters of a string. If a character is a special character, change it to gray
+    */
 }
