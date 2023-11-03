c831dfde1491a9caa0e0c82d1f9d897aefc06efa
Moundier <garamoundier.developer@protonmail.com>
2023-11-03 01:47:23 -0300

    chore: change some colors!

--- src/main/java/com/example/utils/Console.java
+++ src/main/java/com/example/utils/Console.java
@@ -10,15 +10,21 @@ public class Console {
     }
 
     public static class INFO {
-        public static Void log(String message) {
-            System.out.println(addYellowColor("[INFO]: ") + message); // Red
-            return null;
+        public static void log(String message) {
+            System.out.println(addBlueColor("[INFO]: ") + message); // Print info message in blue
         }
     }
 
     public static class WARN {
         public static Void log(String message) {
-            System.out.println(addRedColor("[WARN]: ") + message); // Red
+            System.out.println(addOrangeColor("[WARN]: ") + message); // Orange
+            return null;
+        }
+    }
+
+    public static class FAIL {
+        public static Void log(String message) {
+            System.out.println(addRedColor("[FAIL]: ") + message); // Red
             return null;
         }
     }
@@ -42,8 +48,12 @@ public class Console {
         return "\u001B[32m" + text + removeColor(); // ok
     }
 
-    public static String addYellowColor(String text) {
-        return "\u001B[33m" + text + removeColor();
+    public static String addOrangeColor(String text) {
+        return "\u001B[38;5;208m" + text + removeColor();
+    }
+
+    public static String addBlueColor(String text) {
+        return "\u001B[34m" + text + removeColor(); // Adds blue color
     }
 
     public static String removeColor() {
@@ -54,4 +64,5 @@ public class Console {
         Todo
         Iterate through the characters of a string. If a character is a special character, change it to gray
     */
+
 }
