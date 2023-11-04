fb20cd3a04adda14221e0bf7b82fd573812dee96
Moundier <garamoundier.developer@protonmail.com>
2023-11-04 00:51:09 -0300

    chore: Hint console!

--- src/main/java/com/example/utils/Console.java
+++ src/main/java/com/example/utils/Console.java
@@ -1,51 +1,57 @@
 package com.example.utils;
 
-
 public class Console {
 
     public static class DONE {
         public static void log(String message) {
-            System.out.println(addGreenColor("[DONE]: ") + message); // Red
+            System.out.println(addGreenColor("[DONE]: ") + message);
         }
     }
 
     public static class INFO {
         public static void log(String message) {
-            System.out.println(addBlueColor("[INFO]: ") + message); // Print info message in blue
+            System.out.println(addBlueColor("[INFO]: ") + message);
         }
     }
 
     public static class WARN {
-        public static Void log(String message) {
-            System.out.println(addOrangeColor("[WARN]: ") + message); // Orange
-            return null;
+        public static void log(String message) {
+            System.out.println(addOrangeColor("[WARN]: ") + message);
         }
     }
 
     public static class FAIL {
-        public static Void log(String message) {
-            System.out.println(addRedColor("[FAIL]: ") + message); // Red
-            return null;
+        public static void log(String message) {
+            System.out.println(addRedColor("[FAIL]: ") + message);
         }
     }
 
     public static class LINE {
-        public static Void log(String message) {
-            System.out.println(addGreyColor("[LINE]:") + addGreyColor(" [" + message + "]")); // Grey
-            return null;
+        public static void log(String message) {
+            System.out.println(addGreyColor("[LINE]: [" + message + "]"));
         }
     }
 
+    public static class HINT {
+        public static void log(String message) {
+            System.out.println(addYellowColor("[HINT]: ") + message);
+        }
+    }
+    
+    public static String addYellowColor(String text) {
+        return "\u001B[33m" + text + removeColor();
+    }
+
     public static String addRedColor(String text) {
-        return "\u001B[31m" + text + removeColor(); // ok
+        return "\u001B[31m" + text + removeColor();
     }
 
     public static String addGreyColor(String text) {
-        return "\u001B[90m" + text + removeColor(); // ok
+        return "\u001B[90m" + text + removeColor();
     }
 
     public static String addGreenColor(String text) {
-        return "\u001B[32m" + text + removeColor(); // ok
+        return "\u001B[32m" + text + removeColor();
     }
 
     public static String addOrangeColor(String text) {
@@ -53,7 +59,7 @@ public class Console {
     }
 
     public static String addBlueColor(String text) {
-        return "\u001B[34m" + text + removeColor(); // Adds blue color
+        return "\u001B[34m" + text + removeColor();
     }
 
     public static String removeColor() {
@@ -64,5 +70,4 @@ public class Console {
         Todo
         Iterate through the characters of a string. If a character is a special character, change it to gray
     */
-
 }
